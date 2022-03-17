f3fa1f0b42438d338f8289f0593b61ee0e53a73b
Dominick Moré <dominick.more@outlook.com>
2022-03-17 22:08:49 +0100

    called service emitter unsubscribe

--- src/app/user-status-counter.service.ts
+++ src/app/user-status-counter.service.ts
@@ -1,17 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import UserService, { UserStatus, UserStatusChangeValue } from './user.service';
 
 @Injectable()
-export class UserStatusCounterService {
+
+export class UserStatusCounterService implements OnDestroy {
     
-    private readonly statusCounter: {[key in UserStatus]: number; };
+    private readonly statusCounter: {[key in UserStatus]: number; } = {
+        'active': 0,
+        'inactive': 0
+    };
 
     constructor(private readonly userService: UserService) {
-        this.statusCounter = {
-            'active': 0,
-            'inactive': 0
-        };
         this.userService.statusChangeEmitter.subscribe(this.handleUserStatusChange);
+    }
+
+    ngOnDestroy() {
+        this.userService.statusChangeEmitter.unsubscribe();
     }
 
     private handleUserStatusChange = (value: UserStatusChangeValue) => {
@@ -26,4 +30,6 @@ export class UserStatusCounterService {
     getStatusChanges(status: UserStatus): number {
         return this.statusCounter[status];
     }
+
+    
 }
