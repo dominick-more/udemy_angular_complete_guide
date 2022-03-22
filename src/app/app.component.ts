69c3d55663e409f80fed7fdbbb6250e3bfeb41ba
Dominick Moré <dominick.more@outlook.com>
2022-03-22 18:31:24 +0100

    completed td-assignment-chap-15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,22 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public readonly subscriptions: readonly string[] = ['Basic', 'Advanced', 'Pro']; 
-  @ViewChild('ngForm') public ngForm: NgForm;
+  public readonly subscriptionOptionValues: readonly string[] = ['Basic', 'Advanced', 'Pro']; 
+  public selectedSubscription: string = 'Advanced';
+  public submitted: boolean = false;
+  public submittedData: Record<string, string> = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  @ViewChild('f') public ngForm: NgForm;
 
   onSubmit() {
-    this.ngForm.value;
+    this.submittedData.email = this.ngForm.value.email;
+    this.submittedData.subscription = this.ngForm.value.subscription;
+    this.submittedData.password = this.ngForm.value.password;
+    console.log(this.ngForm.value);
+    this.ngForm.reset();
+    this.submitted = true;
   }
 }
