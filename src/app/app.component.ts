cf2c6b47a229eb5dde3d873c1a52ba998e6714b4
Dominick Moré <dominick.more@outlook.com>
2022-03-22 17:01:32 +0100

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,33 +6,5 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public readonly genders: readonly string[] = ['male', 'female'];
-  @Input() public defaultQuestion: string = 'teacher';
-  @ViewChild('f') public signupForm: NgForm;
-  public questionAnswer: string = '';
-  public submitted: boolean = false;
-  public user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
 
-  onSubmit() {
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-    this.signupForm.reset();
-  }
-  // onSubmit(ngForm: NgForm) {
-  //   console.log(ngForm);
-  // }
 }
